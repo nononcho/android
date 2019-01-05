7eb9a985a764397bf7b13447969b0066fa75286c
nononcho <nononcho@hanmail.net>
2019-01-05 11:51:49 +0900

    SQlite_02

--- app/src/main/java/com/example/administrator/myapplication/SqlListActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/SqlListActivity.java
@@ -1,15 +1,18 @@
 package com.example.administrator.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,6 +22,11 @@ import com.example.administrator.myapplication.sql.UserEntry;
 
 import java.util.ArrayList;
 import java.util.List;
+
+interface _CallBack {
+    void _getData();
+    void _moveMod(long _id);
+}
 
 public class SqlListActivity extends AppCompatActivity {
 
@@ -31,9 +39,23 @@ public class SqlListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sql_list);
 
+        _CallBack cb = new _CallBack() {
+            @Override
+            public void _getData() {
+                getData();
+            }
+
+            @Override
+            public void _moveMod(long _id) {
+                Intent intent = new Intent(SqlListActivity.this, SqlModActivity.class);
+                intent.putExtra("_id", _id);
+                startActivity(intent);
+            }
+        };
+
         setTitle(R.string.title_sqllist);
 
-        adapter = new UserDbAdapter(this, list);
+        adapter = new UserDbAdapter(this, list, cb);
 
         listview = findViewById(R.id.listView); //id값이 listView를 찾는 방법은 위 코드  setContentView(R.layout.activity_sql_list);에서 activity_sql_list로 연결했기때문
         listview.setAdapter(adapter);
@@ -64,6 +86,7 @@ public class SqlListActivity extends AppCompatActivity {
                 null            //The sort order
         );
 
+        list.clear();   //삭제 시 리프레쉬되지 않음. 빼고 해보기
         while (cursor.moveToNext()) {
             long _id = cursor.getLong(cursor.getColumnIndexOrThrow(UserEntry._ID));
             String log_id = cursor.getString(cursor.getColumnIndexOrThrow(UserEntry.LOG_ID));
@@ -80,14 +103,21 @@ public class SqlListActivity extends AppCompatActivity {
 
 class UserDbAdapter extends ArrayAdapter<UserVO> {
     private LayoutInflater inflater;
+    private UserDbHelper dbHelper;
+    private SQLiteDatabase db;
+    private _CallBack cb;
 
-    public UserDbAdapter(@NonNull Context context, @NonNull List<UserVO> list) {
+    public UserDbAdapter(@NonNull Context context, @NonNull List<UserVO> list, _CallBack cb) {
         super(context, 0, list);
         this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        dbHelper = new UserDbHelper(context);
+        db = dbHelper.getWritableDatabase();
+        this.cb = cb;
     }
 
     class ViewHolder {
         TextView tv_id, tv_pw, tv_nm, tv_age;
+        Button btn_mod, btn_del; //onClick 대시에 사용
     }
     @Override
     public View getView(int position, View cv, ViewGroup parent) {
@@ -100,18 +130,63 @@ class UserDbAdapter extends ArrayAdapter<UserVO> {
             vh.tv_pw = cv.findViewById(R.id.tv_pw);
             vh.tv_nm = cv.findViewById(R.id.tv_nm);
             vh.tv_age = cv.findViewById(R.id.tv_age);
+            vh.btn_mod = cv.findViewById(R.id.btn_mod);
+            vh.btn_del = cv.findViewById(R.id.btn_del);
 
             cv.setTag(vh);
         } else {
             vh = (ViewHolder)cv.getTag();
         }
 
-        UserVO vo = getItem(position);
+
+        final UserVO vo = getItem(position);
+        final long _id = vo.get_id();
         vh.tv_id.setText(vo.getLog_id());
         vh.tv_pw.setText(vo.getLog_pw());
         vh.tv_nm.setText(vo.getNm());
         vh.tv_age.setText(Integer.toString(vo.getAge()));
 
+        //삭제 버튼
+        vh.btn_del.setOnClickListener(new View.OnClickListener() {      //인터페이스는  객체와 안됨. but 여기서 객체화 한 것처럼 보임
+            //            vh.btn_del.setOnClickListener(new Click() {   //이렇게 쓸려면 line 141을 정의해주고 써야함.
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+            @Override
+            public void onClick(View view) {
+                Log.i("sbs", "dasfdasdf: " + vo.getNm());   // line 115에 final 붙여야 됨
+                delDB(_id);     //line 120 final 쓰지 않으면 Error 뜸.
+            }
+        });
+
+        //수정 버튼
+        vh.btn_mod.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                cb._moveMod(_id);
+            }
+        });
+
+
         return cv;
+    }
+//    class Click implements View.OnClickListener {     //line 127 보충설명
+//
+//        @Override
+//        public void onClick(View view) {
+//
+//        }
+//    }
+
+    private void delDB(long _id) {
+        String selection = UserEntry._ID  + " = ?";
+        String[] selectionArgs = { Long.toString(_id) };
+
+        int deletedRows = db.delete(UserEntry.T_NM, selection, selectionArgs);
+        if(deletedRows > 0) {
+            cb._getData();
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/myapplication/SqlModActivity.java
@@ -0,0 +1,94 @@
+package com.example.administrator.myapplication;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.administrator.myapplication.sql.UserDbHelper;
+import com.example.administrator.myapplication.sql.UserEntry;
+
+import org.w3c.dom.DOMStringList;
+
+public class SqlModActivity extends AppCompatActivity {
+
+    private EditText et_id, et_pw, et_nm, et_age;
+    private long _id;
+    private  UserDbHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mod_sql);
+
+        setTitle(R.string.title_sqlmod);
+
+//        UserDbHelper dbHelper = new UserDbHelper(getBaseContext());
+//        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
+        et_nm = findViewById(R.id.et_nm);
+        et_age = findViewById(R.id.et_age);
+
+        dbHelper = new UserDbHelper(SqlModActivity.this);
+
+        Intent intent = getIntent();
+        _id = intent.getLongExtra("_id", 0);
+        getData(_id);
+
+
+    }
+
+    private void getData(long _id) {
+        if(_id > 0) {
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            String query = "SELECT log_id, log_pw, nm, age FROM " + UserEntry.T_NM + " WHERE _id = " + _id;
+            Cursor cursor = db.rawQuery(query, null);
+
+            if(cursor.moveToNext()) {
+                String log_id = cursor.getString(cursor.getColumnIndexOrThrow(UserEntry.LOG_ID));
+                String log_pw = cursor.getString(cursor.getColumnIndexOrThrow(UserEntry.LOG_PW));
+                String nm = cursor.getString(cursor.getColumnIndexOrThrow(UserEntry.NM));
+                int age = cursor.getInt(cursor.getColumnIndexOrThrow(UserEntry.AGE));
+
+                et_id.setText(log_id);
+                et_pw.setText(log_pw);
+                et_nm.setText(nm);
+                et_age.setText(Integer.toString(age));
+            }
+        }
+    }
+
+    public void clkSave(View v) {
+        String id = et_id.getText().toString();
+        String pw = et_pw.getText().toString();
+        String nm = et_nm.getText().toString();
+        String age = et_age.getText().toString();
+        int iage = Integer.parseInt(age);
+
+        UserDbHelper dbHelper = new UserDbHelper(getBaseContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        //String query = "UPDATE " + UserEntry.T_NM + " SET " + UserEntry.LOG_ID + " = '" + _id + "' " + " , log_pw = '" + pw + "' " + " , nm = '" + nm + "' " + " , age = '" + age;
+
+        ContentValues values = new ContentValues();
+        values.put(UserEntry.LOG_ID, id);
+        values.put(UserEntry.LOG_PW, pw);
+        values.put(UserEntry.NM, nm);
+        values.put(UserEntry.AGE, iage);
+
+        String selection = UserEntry._ID + " = ?";
+        String[] selectionArgs = { Long.toString(_id)};
+
+        int count = db.update( UserEntry.T_NM, values, selection, selectionArgs);
+
+        finish();
+
+    }
+
+}
